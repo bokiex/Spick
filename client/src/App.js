591bcc53c6e5495fcb608be69a0fc77810904bd3
John Koh <kohbokyan@gmail.com>
2024-02-08 13:13:39 +0800

    Fix link between react and flask

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,32 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import React, { useState, useEffect } from "react";
 function App() {
-    const { data, setData } = useState([{}]);
+    const [data, setData] = useState([{}]);
 
+    // simple fetch request to the server
     useEffect(() => {
         fetch("/members")
             .then((res) => res.json())
-            .then((data) => setData(data));
-    });
-    return <div></div>;
+            .then((data) => {
+                setData(data);
+                console.log(data);
+            });
+    }, []);
+    return (
+        <div>
+            {typeof data.members === "undefined" ? (
+                <h1>Loading...</h1>
+            ) : (
+                data.members.map((member, index) => {
+                    return (
+                        <div key={index}>
+                            <h1>{member}</h1>
+                        </div>
+                    );
+                })
+            )}
+        </div>
+    );
 }
 
 export default App;
