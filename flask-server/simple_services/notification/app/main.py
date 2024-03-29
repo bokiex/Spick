ff0cbe9906b398d777a746d2ef3bb18fae3e93b8
John Koh <kohbokyan@gmail.com>
2024-03-30 01:54:48 +0800

    fixed notification... FOR THE FINAL TIME

--- flask-server/simple_services/notification/app/main.py
+++ flask-server/simple_services/notification/app/main.py
@@ -2,6 +2,8 @@ import sqlalchemy as db
 import requests
 import amqp_connection
 import json
+import threading
+import asyncio
 from telebot import TeleBot
 from os import environ
 from flask import jsonify
@@ -14,8 +16,6 @@ bot_token = environ.get('BOT_TOKEN') or "6996801409:AAGDWkgPaCtRAqH08y9lwYJQif6E
 user_ms = environ.get("USER_URL") or "http://localhost:3000/users/"
 notification_ms = environ.get("NOTIFICATION_URL") or "http://localhost:5000/notification/"
 bot = TeleBot(bot_token)
-
-app = FastAPI()
 
 def receiver():
     connection = amqp_connection.create_connection()
@@ -40,7 +40,7 @@ def receiver():
         if users.status_code in range(300, 599):
             return JSONResponse(status_code=users.status_code, content={"message":users.json()["detail"]})
         users = users.json()
-        print(users)
+    
         for user in users:
             
             if user['telegram_id'] == None or user['telegram_tag'] not in notification_list:
@@ -55,7 +55,33 @@ def receiver():
     channel.basic_consume(queue="Notification", on_message_callback=callback, auto_ack=True)
     channel.start_consuming()
 
-@bot.message_handler(func=receiver())
-def echo_msg(message):
-    pass
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    
+    telegram_id = message.chat.id
+    telegram_tag = message.chat.username
+    msg = ""
+        
+    user = requests.get(user_ms + f"telegram/{telegram_tag}")
+    if user.status_code in range(300, 599):
+        msg = "User not found. Have you created an account yet?"
+        return bot.send_message(telegram_id, msg)
+    
+    user = user.json()
+    user["telegram_id"] = str(telegram_id)
+    print(user)
+    update = requests.put(user_ms + f"{user['user_id']}", json=jsonable_encoder(user))
+    
+    if update.status_code in range(300, 599):
+        msg = "An error occurred updating the user."
+    msg = "Your account is now tied to your telegram tag."
+    return bot.send_message(telegram_id, msg)
 
+if __name__ == "__main__":
+    print("Starting AMQP thread")
+    receiver_thread = threading.Thread(target=receiver)
+    receiver_thread.start()
+
+    print("Starting Telegram bot thread")
+    bot.polling()
+    receiver_thread.join()
--- flask-server/simple_services/user/app/main.py
+++ flask-server/simple_services/user/app/main.py
@@ -10,45 +10,7 @@ from telebot.async_telebot import AsyncTeleBot
 from os import environ
 from contextlib import asynccontextmanager
 
-bot_token = environ.get('BOT_TOKEN') or "6996801409:AAGDWkgPaCtRAqH08y9lwYJQif6ESOnQ984"
-bot = AsyncTeleBot(bot_token)
-
-@bot.message_handler(func=lambda m: True)
-async def create_user(message):
-    telegram_id = message.chat.id
-    telegram_tag = message.chat.username
-    msg = ""
-    with database.SessionManager() as db:
-        telegram_tag = "@" + telegram_tag
-        
-        user = crud.get_user_by_telegram_tag(db, telegram_tag)
-        if user is None:
-            msg = "User not found. Have you created an account yet?"
-        
-        
-        user.telegram_id = str(telegram_id)
-        print(jsonable_encoder(user))
-        result = crud.update_user(db, user, user.user_id)
-        
-        if result is None:
-            msg = "An error occurred updating the user."
-        msg = "Your account is now tied to your telegram tag."
-
-
-    await bot.send_message(telegram_id, msg)
-    
-    
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    bot_task = asyncio.create_task(bot.polling())
-    print("Bot started.")
-    
-    yield
-
-    print("Stopping bot...")
-    bot_task.cancel()
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 origins = [
     "http://localhost:5173",
@@ -107,7 +69,7 @@ async def get_user_by_telegram_tag(telegram_tag: str, db: Session = Depends(get_
     return jsonable_encoder(result)
 
 # Update user
-@app.put("/users/user_id/{user_id}")
+@app.put("/users/{user_id}")
 async def update_user(user: schemas.User, user_id: int, db: Session = Depends(get_db)):
     result = crud.update_user(db, user, user_id)
     if result is None:
