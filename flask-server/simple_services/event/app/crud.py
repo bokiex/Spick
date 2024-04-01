97606ca6fd36f2a59c64f7fea1307f4c1641360b
John Koh <kohbokyan@gmail.com>
2024-04-01 23:33:53 +0800

    merge and add volume to mysql dbs in compose

--- flask-server/simple_services/event/app/crud.py
+++ flask-server/simple_services/event/app/crud.py
@@ -74,7 +74,7 @@ def get_invitee_responded(db: Session, event_id: str):
 def update_invitee(db: Session, invitee: schemas.Invitee):
     db_invitee = db.query(models.Invitee).filter(models.Invitee.user_id == invitee.user_id, models.Invitee.event_id == invitee.event_id).first()
     if db_invitee:
-        db_invitee.status = invitee.status
+        setattr(db_invitee, 'status', invitee.status)
         db.commit()
         db.refresh(db_invitee)
     return db_invitee
